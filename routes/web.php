993bc03599b9a7f2dcb3d09528b418e944289d2e
orlando manfredini <orlando.manfre1@gmail.com>
2024-05-30 19:32:13 +0200

    ADD: resource (ci tengo a precisare che questi commit li ho fatti guardando le slide/ lezione di stamattina )

--- routes/web.php
+++ routes/web.php
@@ -18,16 +18,24 @@ Route::get('/', function () {
     return view('layout');
 });
 
-Route::get('/comics', [ComicController::class, 'index'])->name('comics.index');
+Route::resource('comics',ComicController::class);
 
-Route::get('/comics/{comic}', [ComicController::class, 'show'])->name('comics.show')->where('comic', '[0-9]+');
 
-Route::get('/comics/create', [ComicController::class, 'create'])->name('comics.create');
+// php artisan route:list
 
-Route::post('/comics', [ComicController::class, 'store'])->name('comics.store');
 
-Route::get('/comics/{comic}/edit', [ComicController::class, 'edit'])->name('comics.edit');
 
-Route::put('/comics/{comic}',[ComicController::class, 'update'])->name('comics.update');
 
-Route::delete('/comics{comic}', [ComicController::class, 'destroy'])->name('comics.destroy');
+// Route::get('/comics', [ComicController::class, 'index'])->name('comics.index');
+
+// Route::get('/comics/{comic}', [ComicController::class, 'show'])->name('comics.show')->where('comic', '[0-9]+');
+
+// Route::get('/comics/create', [ComicController::class, 'create'])->name('comics.create');
+
+// Route::post('/comics', [ComicController::class, 'store'])->name('comics.store');
+
+// Route::get('/comics/{comic}/edit', [ComicController::class, 'edit'])->name('comics.edit');
+
+// Route::put('/comics/{comic}',[ComicController::class, 'update'])->name('comics.update');
+
+// Route::delete('/comics{comic}', [ComicController::class, 'destroy'])->name('comics.destroy');
